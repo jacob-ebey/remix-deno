f8a83ebcaedf6c36c64d3365e1b7b950ce43e825
Jacob Ebey <jacob.ebey@live.com>
2022-06-15 21:45:37 -0700

    organize imports

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
+export { loadConfig } from "./runtime/config.ts";
 export { serveDev, writeRemixGen } from "./runtime/dev.ts";
 export { serveProd } from "./runtime/prod.ts";
-
-export { loadConfig } from "./runtime/config.ts";
