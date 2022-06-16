c394ad3ee682cb1800bc60003a837da92bbccf4b
Jacob Ebey <jacob.ebey@live.com>
2022-06-15 21:57:03 -0700

    added brotli compression

--- runtime/serve.ts
+++ runtime/serve.ts
@@ -1,12 +1,19 @@
-import { mediaTypeLookup, server } from "../deps.ts";
+import { brotliCompress, mediaTypeLookup, server } from "../deps.ts";
 
 export async function serve({
   staticAssets,
   remixRequestHandler,
 }: {
-  staticAssets: Map<string, string>;
+  staticAssets: Map<string, string | Uint8Array>;
   remixRequestHandler: (request: Request) => Promise<Response>;
 }) {
+  const encoder = new TextEncoder();
+  for (let [key, asset] of staticAssets) {
+    if (typeof asset === "string") {
+      asset = encoder.encode(asset);
+    }
+    staticAssets.set(key, brotliCompress(asset));
+  }
   async function requestHandler(
     request: Request
     // connectionInfo: server.ConnInfo
@@ -17,6 +24,7 @@ export async function serve({
       const headers = new Headers();
       const contentType = mediaTypeLookup(url.pathname);
       contentType && headers.set("Content-Type", contentType);
+      headers.set("Content-Encoding", "br");
       return new Response(staticAsset, { headers });
     }
 
